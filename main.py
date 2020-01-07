5a01abb4af057145640d0ada1c62f79d12055730
Yuya Sugano <sugano.yuuya@gmail.com>
2020-01-07 17:42:58 +0900

    add .gitignore

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import time
 
 def tpot_classifier(X, y):
     X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, random_state=39)
-    pipeline_optimizer = TPOTClassifier(generations=10, population_size=100, cv=5, random_state=42, verbosity=2)
+    pipeline_optimizer = TPOTClassifier(generations=10, population_size=10, cv=5, random_state=42, verbosity=2)
     pipeline_optimizer.fit(X_train, y_train)
+    pipeline_optimizer.export('tpot_pipeline.py')
     print("Accuracy score", pipeline_optimizer.score(X_test, y_test))
 
     return pipeline_optimizer
